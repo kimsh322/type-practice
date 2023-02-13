982ce0ecef8dfe66d04da3b29fb2166c1ec80502
kimsh322 <kimsh91322@gmail.com>
2023-02-13 16:36:47 +0900

    기본이름수정

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,9 @@
+// 입장 페이지 키다운 이벤트 구현입니다.
+
 const nameInput = document.querySelector("#name-input");
 const container = document.querySelector("#container");
 const loader = document.querySelector("#loader");
-let username = "잠둘기";
+let username = "바보";
 nameInput.onkeydown = (e) => {
   if (e.key === "Enter") {
     username = e.target.value ? e.target.value : username;
